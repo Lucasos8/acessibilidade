76910d83613d3af9ba4ac1c4a13dad08d9dd9520
ocai0 <caioalvesfernandes@gmail.com>
2023-11-10 23:26:43 -0300

    Added routes for atendente

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::view('/request-type', '/request-type');
 Route::post("/login", "App\Http\Controllers\Usuario\ClienteController@login")->name("user.login");
 Route::post("/save-room", "App\Http\Controllers\Usuario\ClienteController@saveRoomId")->name("user.saveRoom");
 Route::post("/request-live-session", "App\Http\Controllers\VideoChat\VideoChatController@createRequest")->name("videoChat.createRoom");
+
+Route::post("atendente/login", "App\Http\Controllers\Usuario\AtendenteController@login")->name("attendant.login");
+Route::post("atendente/salas-abertas", "App\Http\Controllers\Usuario\AtendenteController@listRooms")->name("attendant.listRooms");
+Route::post("atendente/sala/{id}", "App\Http\Controllers\Usuario\AtendenteController@accessRoom")->name("attendant.accessRoom");
